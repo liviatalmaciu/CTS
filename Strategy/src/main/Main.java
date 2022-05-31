f33cb2759eac1275e99c87bf5b6d5c65839b84e1
Livia-Ioana Talmaciu <73893318+liviatalmaciu@users.noreply.github.com>
2022-05-31 23:43:22 +0300

    Add files via upload

---
+++ Strategy/src/main/Main.java
@@ -0,0 +1,21 @@
+
+package main;
+
+import strategy.CardBancar;
+import strategy.Sms;
+import strategy.ValidatorCalatorie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ValidatorCalatorie validatorCalatorie=new ValidatorCalatorie(3);
+        validatorCalatorie.platesteCalatorie();
+
+        validatorCalatorie.setModPlata(new CardBancar());
+        validatorCalatorie.platesteCalatorie();
+        validatorCalatorie.setModPlata(new Sms());
+        validatorCalatorie.platesteCalatorie();
+
+    }
+
+}
---
+++ Strategy/src/strategy/ValidatorCalatorie.java
@@ -0,0 +1,23 @@
+
+package strategy;
+
+public class ValidatorCalatorie {
+
+    private ModPlata modPlata;
+    private double pret;
+
+    public ValidatorCalatorie(double pret) {
+        this.pret=pret;
+        this.modPlata=new CardCalatorii();
+    }
+
+    public void setModPlata(ModPlata modPlata) {
+        this.modPlata = modPlata;
+    }
+
+    public void platesteCalatorie() {
+        modPlata.plateste(pret);
+    }
+
+
+}
